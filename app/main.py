29e6d7f2b5d77c432b7ef45e294a42163fb46053
mzaoualim <74496123+mzaoualim@users.noreply.github.com>
2024-04-16 13:41:33 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -48,28 +48,24 @@ def main():
   st.write('---')
   
   # Ask Gemini
-  a, b = st.columns(2)
   st.markdown("<h2 style='text-align: center;'> Ask </h2>", unsafe_allow_html=True)
-  with a:
-    question = st.text_input('Ask Gemini about Senatorial Stance', 'debt ceiling')
-
-  with b:
-    #Button for submit
-    ask = st.button('Response?', use_container_width=True)
+  question = st.text_input('Ask Gemini about Senatorial Stance', 'debt ceiling')
+  #Button for submit
+  ask = st.button('Response?', use_container_width=True)
 
   #When button is clicked
   if ask:
     response = get_sen_stance(documents, question)
-  st.write('---')
-    
-  # Results
-  st.markdown("<h2 style='text-align: center;'> Senators Lists </h2>", unsafe_allow_html=True)
-  # st.expande 4 cols (support/opposition/neutrals/Undecisive)
-  st.write(response)
-  st.write('---')
-
-  st.markdown("<h2 style='text-align: center;'> PieChart </h2>", unsafe_allow_html=True)
-  st.write('---')
+    st.write('---')
+      
+    # Results
+    st.markdown("<h2 style='text-align: center;'> Senators Lists </h2>", unsafe_allow_html=True)
+    # st.expande 4 cols (support/opposition/neutrals/Undecisive)
+    st.write(response)
+    st.write('---')
+  
+    st.markdown("<h2 style='text-align: center;'> PieChart </h2>", unsafe_allow_html=True)
+    st.write('---')
 
 if __name__ == '__main__':
   main()
